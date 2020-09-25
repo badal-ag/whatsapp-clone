98fa166706e84296bd4b6b49cd53328d0c797f83
Badal Agarwal <badal.agarwal1369@gmail.com>
2020-09-25 23:19:37 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="Visit Github"
+          href="https://github.com/badal-ag"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           Visit Github
         </a>
       </header>
     </div>
